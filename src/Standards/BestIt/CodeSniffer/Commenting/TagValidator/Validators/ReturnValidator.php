a7c95245d0c09dea7f978cf08709b0c94f8ef080
Björn Lange <code@b3nl.de>
2018-09-10 10:39:45 +0200

    Added todo tag

--- src/Standards/BestIt/CodeSniffer/Commenting/TagValidator/Validators/ReturnValidator.php
+++ src/Standards/BestIt/CodeSniffer/Commenting/TagValidator/Validators/ReturnValidator.php
@@ -54,6 +54,7 @@ class ReturnValidator extends AbstractValidator
             return true;
         }
 
+        // TODO This would support whitespace(s) as valid comment. Use count(explode) >= 2 or regex.
         if (substr_count($content, ' ') >= 1) {
             return true;
         }
